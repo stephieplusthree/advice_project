3834db6ec5fe5c8156ba302defe82fecce6271a3
stephanie <stephieplusthree@gmail.com>
2020-08-30 08:37:57 -0700

    finished product

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <h1>APP</h1>
-  );
+import './App.css';
+import { render } from '@testing-library/react';
+
+class App extends React.Component {
+  state = { 
+    advice: '',
+  }
+
+  componentDidMount() {
+    //call the method
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
